065557ff6b563c25cc7a6ee33730793ebf51a14a
Gschpektor1 <119686016+Gschpektor1@users.noreply.github.com>
2022-12-22 15:30:58 -0500

    fix

--- block.py
+++ block.py
@@ -6,17 +6,17 @@ class Block:
 		self.length = dimensions[1]
 		self.height = dimensions[2]
 
-	def get_width():
+	def get_width(self):
 		return self.width
 
-	def get_length():
+	def get_length(self):
 		return self.length
 
-	def get_height():
+	def get_height(self):
 		return self.height
 
-	def get_volume():
+	def get_volume(self):
 		return self.length*self.width*self.height
 
-	def get_surface_area():
+	def get_surface_area(self):
 		return (self.height*self.width)*6
